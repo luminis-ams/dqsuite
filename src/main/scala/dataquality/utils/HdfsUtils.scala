c11a8abdd6dedf1dd951a4c21b03efa4f164d99b
Egor Dmitriev <egor.dmitriev@luminis.eu>
2023-03-30 11:34:34 +0200

    feat: Added Hdfs utils for loading files from s3, hdfs or local filesystem

---
+++ src/main/scala/dataquality/utils/HdfsUtils.scala
@@ -0,0 +1,32 @@
+package dataquality.utils
+
+import org.apache.hadoop.fs.{FSDataInputStream, FileSystem, Path}
+import org.apache.spark.sql.SparkSession
+
+
+object HdfsUtils {
+  /* Make sure we write to the correct filesystem, as EMR clusters also have an internal HDFS */
+  def asQualifiedPath(session: SparkSession, path: String): (FileSystem, Path) = {
+    val hdfsPath = new Path(path)
+    val fs = hdfsPath.getFileSystem(session.sparkContext.hadoopConfiguration)
+    val qualifiedPath = hdfsPath.makeQualified(fs.getUri, fs.getWorkingDirectory)
+
+    (fs, qualifiedPath)
+  }
+
+  /* Helper function to read from a binary file on S3 */
+  def readFromFileOnDfs[T](session: SparkSession, path: String)
+                          (readFunc: FSDataInputStream => T): T = {
+
+    val (fs, qualifiedPath) = asQualifiedPath(session, path)
+    val input = fs.open(qualifiedPath)
+
+    try {
+      readFunc(input)
+    } finally {
+      if (input != null) {
+        input.close()
+      }
+    }
+  }
+}
